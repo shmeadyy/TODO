fd3d7819d9f4ea6dc19fa2f2b15a713f566ad3c7
Laura Mead <lauraemead7@gmail.com>
2014-10-07 15:48:02 -0700

    refactor todo list creation in spec

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -2,18 +2,17 @@ require 'spec_helper'
 
 describe "Creating todo lists" do
 	def create_todo_list(options={})
-		options[:title] ||= "My todo list"
-		options[:description] ||= "The test description: This is my todo list"
+		options[:title] ||= "My Todo List"
+		options[:description] ||= "This is my todo list."
 
 		visit "/todo_lists"
 		click_link "New Todo list"
 		expect(page).to have_content("New todo_list")
 
 		fill_in "Title", with: options[:title]
-		fill_in "Desription", with: options[:description]
+		fill_in "Description", with: options[:description]
 		click_button "Create Todo list"
 	end
-
 
 	it "redirects to the todo list index page on success" do
 		create_todo_list
