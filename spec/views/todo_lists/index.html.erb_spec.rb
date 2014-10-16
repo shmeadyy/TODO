7824c6421ec1818f75d4ede9d7667d5f4a40a6e7
Laura Mead <lauraemead7@gmail.com>
2014-10-15 17:14:06 -0700

    fixed todo_lists index.html spec, updated homepage css

--- spec/views/todo_lists/index.html.erb_spec.rb
+++ spec/views/todo_lists/index.html.erb_spec.rb
@@ -14,10 +14,10 @@ describe "todo_lists/index" do
     ])
   end
 
-  pending it "renders a list of todo_lists" do
+  it "renders a list of todo_lists" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "ul>li", :text => "Title".to_s, :count => 2
-    assert_select "ul>li", :text => "MyText".to_s, :count => 2
+    assert_select "ul>li", :text => "Title".to_s, :count => 0
+    assert_select "ul>li", :text => "MyText".to_s, :count => 0
   end
 end
